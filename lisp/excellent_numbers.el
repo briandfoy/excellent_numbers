72e07b78814e3568ebd92ca05bbc9cc415b7ae03
brian d foy <brian.d.foy@gmail.com>
2016-08-15 03:58:41 -0400

    Do the odd powers in the lisp program

--- lisp/excellent_numbers.el
+++ lisp/excellent_numbers.el
@@ -2,11 +2,13 @@
 	"Determine is ab = b^2 - a^2"
 	(let ((ab-string (write-to-string number)))
 		(let ((ab-length (length ab-string)))
-			(format t "String: ~a~%Length: ~a~%" ab-string ab-length)
 			(let ((a (parse-integer (subseq ab-string 0 (/ ab-length 2))))
 			      (b (parse-integer (subseq ab-string (/ ab-length 2)))))
-			      	(format t "a: ~a~%b: ~a~%" a b)
 			      	(if (= (- (* b b) (* a a )) number) t nil)
 	))))
 
-(princ (is-excellent 3468))
+(loop for n from 0 to 5
+	do (loop for ab from (expt 10 (+ (* n 2) 1 ) ) to (- (expt 10 (+ (+ (* n 2) 1 ) 1)) 1 )
+		do (if (is-excellent ab) (format t "~a~%" ab))
+		)
+	)
